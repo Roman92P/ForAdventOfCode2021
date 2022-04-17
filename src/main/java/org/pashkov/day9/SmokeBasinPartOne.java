279f8c3c262a4708926555db0ba8bc32e382b060
Roman <roman.paszkow@gmail.com>
2022-04-17 23:07:09 +0200

    finished day 9_1

--- src/main/java/org/pashkov/day9/SmokeBasinPartOne.java
+++ src/main/java/org/pashkov/day9/SmokeBasinPartOne.java
@@ -4,77 +4,104 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class SmokeBasinPartOne {
     public static void main(String[] args) {
 
-        Map<String, List<String>> pointsMap = getPointAndAllAroundNumbers();
+        Map<String, List<String>> pointsMap = getPointAndAllAroundNumbers2();
 
-        for (Map.Entry<String, List<String>> entry : pointsMap.entrySet()) {
-            String key = entry.getKey();
-            List<String> value = entry.getValue();
+        printMapContent(pointsMap);
+
+        int i = calculateRisk(pointsMap);
+
+        System.out.println(i);
+
+    }
+
+    private static int calculateRisk(Map<String, List<String>> lowestPoints){
+        int result = 0;
+        for (Map.Entry<String, List<String>> entry : lowestPoints.entrySet()){
+            String s = entry.getKey().substring(0,1);
+            System.out.println(s);
+            result = result + Integer.parseInt(s)+1;
+        }
+        return result;
+    }
+
+    private static void printMapContent(Map<?, ?> mapToBePrinted){
+        for (Map.Entry<?, ?> entry : mapToBePrinted.entrySet()) {
+            Object key = entry.getKey();
+            Object value = entry.getValue();
             System.out.println(key + " : " + value);
         }
     }
 
-    private static Map<String, List<String>> getPointAndAllAroundNumbers() {
+    private static Map<String, List<String>> getPointAndAllAroundNumbers2() {
         Map<String, List<String>> result = new HashMap<>();
         List<String> strings = readInputFromFile();
         String[][] tempArray = convertTo2DArray(strings);
 
         for (int i = 0; i < tempArray.length; i++) {
-            String toCompare = tempArray[i][0];
-            for (int j = 1; j < tempArray[i].length; j++) {
-                boolean addedPrevious = false;
-                if (Integer.parseInt(tempArray[i][j]) < Integer.parseInt(toCompare)) {
-                    toCompare = tempArray[i][j];
-                    if((j+1)>=tempArray[i].length){
-                        String mapKey = tempArray[i][j];
-                        List<String> temList = new ArrayList<>();
-                        temList.add(tempArray[i][j-1]);
-                        if ((i + 1) < tempArray.length) {
-                            temList.add(tempArray[i + 1][j - 1]);
-                            if (addedPrevious) {
-                                temList.add(tempArray[i + 1][j - 2]);
-                            }
-                            temList.add(tempArray[i + 1][j]);
-                        }
-                        if ((i - 1) >= 0) {
-                            temList.add(tempArray[i - 1][j - 1]);
-                            if (addedPrevious) {
-                                temList.add(tempArray[i - 1][j - 2]);
-                            }
-                            temList.add((tempArray[i-1][j]));
-                        }
-                        result.put(mapKey, temList);
-                    }
-                } else {
-                    String mapKey = tempArray[i][j - 1];
-                    List<String> temList = new ArrayList<>();
-                    if (j > 1) {
-                        temList.add(tempArray[i][j - 2]);
-                        addedPrevious = true;
-                    }
-                    temList.add(tempArray[i][j]);
-                    if ((i + 1) < tempArray.length) {
-                        temList.add(tempArray[i + 1][j - 1]);
-                        if (addedPrevious) {
-                            temList.add(tempArray[i + 1][j - 2]);
-                        }
-                        temList.add(tempArray[i + 1][j]);
-                    }
-                    if ((i - 1) >= 0) {
-                        temList.add(tempArray[i - 1][j - 1]);
-                        if (addedPrevious) {
-                            temList.add(tempArray[i - 1][j - 2]);
-                        }
-                        temList.add((tempArray[i-1][j]));
-                    }
-                    result.put(mapKey, temList);
+            for (int j = 0; j < tempArray[i].length; j++) {
+                String workingStr = tempArray[i][j];
+                List<String> temList = new ArrayList<>();
+
+                List<String> neighbours = new ArrayList<>();
+                if(i != 0 && j != 0 && i < tempArray.length-1 && j < tempArray[i].length-1){
+                    //neighbours.add(tempArray[i-1][j-1]);
+                    neighbours.add(tempArray[i-1][j]);
+                    //neighbours.add(tempArray[i-1][j+1]);
+                    neighbours.add(tempArray[i][j-1]);
+                    neighbours.add(tempArray[i][j+1]);
+                    //neighbours.add(tempArray[i+1][j-1]);
+                    neighbours.add(tempArray[i+1][j]);
+                    //neighbours.add(tempArray[i+1][j+1]);
+                } else if(i == 0 && j != 0 && j < tempArray[i].length-1){
+                    neighbours.add(tempArray[i][j-1]);
+                    neighbours.add(tempArray[i][j+1]);
+                    neighbours.add(tempArray[i+1][j]);
+                    //neighbours.add(tempArray[i+1][j-1]);
+                    //neighbours.add(tempArray[i+1][j+1]);
+                } else if (i == tempArray.length-1 && j != 0 && j < tempArray[i].length-1){
+                    neighbours.add(tempArray[i][j-1]);
+                    neighbours.add(tempArray[i][j+1]);
+                    neighbours.add(tempArray[i-1][j]);
+                    //neighbours.add(tempArray[i-1][j+1]);
+                    //neighbours.add(tempArray[i-1][j-1]);
+                } else if (i == 0 && j == 0){
+                    neighbours.add(tempArray[i][j+1]);
+                    neighbours.add(tempArray[i+1][j]);
+                    //neighbours.add(tempArray[i+1][j+1]);
+                }else if(i == 0 && j == tempArray[i].length-1){
+                    neighbours.add(tempArray[i][j-1]);
+                    neighbours.add(tempArray[i+1][j]);
+                    //neighbours.add(tempArray[i+1][j-1]);
+                }else if (i == tempArray.length-1 && j == 0){
+                    neighbours.add(tempArray[i][j+1]);
+                    neighbours.add(tempArray[i-1][j]);
+                    //neighbours.add(tempArray[i-1][j+1]);
+                }else if (i == tempArray.length-1 && j == tempArray[i].length-1){
+                    neighbours.add(tempArray[i][j-1]);
+                    neighbours.add(tempArray[i-1][j]);
+                   //neighbours.add(tempArray[i-1][j-1]);
+                } else if (i !=0 && i < tempArray.length-1 && j ==0){
+                    neighbours.add(tempArray[i-1][j]);
+                    //neighbours.add(tempArray[i-1][j+1]);
+                    neighbours.add(tempArray[i][j+1]);
+                    neighbours.add(tempArray[i+1][j]);
+                    //neighbours.add(tempArray[i+1][j+1]);
+                } else if (i !=0 && i < tempArray.length-1 && j == tempArray[i].length-1){
+                    neighbours.add(tempArray[i-1][j]);
+                    //neighbours.add(tempArray[i-1][j-1]);
+                    neighbours.add(tempArray[i][j-1]);
+                    neighbours.add(tempArray[i+1][j]);
+                    //neighbours.add(tempArray[i+1][j-1]);
+                }
+                int minNeighbour = neighbours.stream().mapToInt(Integer::parseInt).min().orElseThrow(NoSuchElementException::new);
+                if(Integer.parseInt(workingStr)<minNeighbour){
+                    UUID uuid = UUID.randomUUID();
+                    result.put(workingStr+"."+uuid, neighbours);
                 }
             }
         }
@@ -91,7 +118,7 @@ public class SmokeBasinPartOne {
 
     // read input
     public static List<String> readInputFromFile() {
-        Path path = Paths.get("input9.txt");
+        Path path = Paths.get("input9_1.txt");
         try {
             return Files.readAllLines(path);
         } catch (IOException e) {
