c1c9f78ec839524eccd85ee56c4c6a50e437e913
Roman92P <roman.paszkow@gmail.com>
2022-06-14 23:24:02 +0200

    Started day 11, part one

--- src/main/java/org/pashkov/day11/DumboOctopusPartOne.java
+++ src/main/java/org/pashkov/day11/DumboOctopusPartOne.java
@@ -1,8 +1,11 @@
 package org.pashkov.day11;
 
+import org.apache.commons.lang3.ArrayUtils;
 import org.pashkov.AoCUtills.AoCInputReader;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Roman Pashkov created on 13.06.2022 inside the package - org.pashkov.day11
@@ -10,6 +13,35 @@ import java.util.List;
 public class DumboOctopusPartOne {
     public static void main(String[] args) {
         List<String> strings = AoCInputReader.readInputFromFile("input11_1.txt");
-        System.out.println(strings);
+        int count = 0;
+        while (count < 100) {
+            strings = increaseEachCharacterByOne(strings);
+            printListWithLines(strings);
+            System.out.println("-----------------------");
+            count++;
+        }
+    }
+
+    private static List<String> increaseEachCharacterByOne(List<String> strings) {
+        return strings.stream()
+                .map(String::toCharArray)
+                .map(chars -> {
+                    for (int i = 0; i < chars.length; i++) {
+                        char aChar = chars[i];
+                        int i1 = Integer.parseInt(String.valueOf(aChar));
+                        i1 = i1 + 1;
+                        if(i1>9) i1 = 0;
+                        chars[i] = String.valueOf(i1).toCharArray()[0];
+                    }
+                    return chars;
+                })
+                .map(chars -> Arrays.toString(chars).replaceAll("[\\]\\[,\\s]", ""))
+                .collect(Collectors.toList());
+    }
+
+    private static void printListWithLines(List<String> strings) {
+        for (String s : strings) {
+            System.out.println(s);
+        }
     }
 }
