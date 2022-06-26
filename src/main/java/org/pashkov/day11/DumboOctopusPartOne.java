5c4fa5e94d28c7353b996ebbb6043a7b27eadcb2
Roman92P <roman.paszkow@gmail.com>
2022-06-26 15:50:41 +0200

    day 11, part one

--- src/main/java/org/pashkov/day11/DumboOctopusPartOne.java
+++ src/main/java/org/pashkov/day11/DumboOctopusPartOne.java
@@ -1,9 +1,11 @@
 package org.pashkov.day11;
 
+import org.apache.commons.lang3.ArrayUtils;
 import org.pashkov.AoCUtills.AoCInputReader;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -26,53 +28,61 @@ public class DumboOctopusPartOne {
         List<String> strings = AoCInputReader.readInputFromFile("input11_2.txt");
         int count = 0;
         int flashes = 0;
-        while (count < 100) {
+        while (count < 2) {
             strings = increaseEachCharacterByOne(strings);
             printListWithLines(strings);
+            strings = correctByZero(strings);
+            System.out.println("All flashes: " + flashes);
             int tempFlashes = countFlashes(strings);
-            System.out.println("New flashes: "+tempFlashes);
+            System.out.println("New flashes: " + tempFlashes);
             flashes = flashes + tempFlashes;
-            System.out.println("All flashes: "+ flashes);
-            System.out.println("-----------------------");
-             correctByZero(strings);
             count++;
+            System.out.println("-----------------------");
         }
     }
 
     private static List<String> correctByZero(List<String> strings) {
-      int [][] array =  new int[strings.size()][strings.get(0).length()];
-        for (int i = 0; i < strings.size(); i++) {
-            char[] chars = strings.get(i).toCharArray();
-            for (int j = 0; j < chars.length; j++) {
-                array[i][j] = Character.getNumericValue(chars[j]);
-                int toCheck = array[i][j];
-                if(toCheck==0){
-                    try{
-                        if(array[i][j+1] != 0)
-                            array[i][j+1] = Character.getNumericValue(array[i][j+1])+1;
-                        if(array[i][j-1] != 0)
-                            array[i][j-1] = Character.getNumericValue(array[i][j-1])+1;
-                        if(array[i+1][j] != 0)
-                            array[i+1][j] = Character.getNumericValue(array[i+1][j])+1;
-                        if(array[i-1][j] != 0)
-                            array[i-1][j] = Character.getNumericValue(array[i-1][j])+1;
-                        if(array[i+1][j-1] != 0)
-                            array[i+1][j-1] = Character.getNumericValue(array[i+1][j-1])+1;
-                        if(array[i+1][j+1] != 0)
-                            array[i+1][j+1] = Character.getNumericValue(array[i+1][j+1])+1;
-                        if(array[i-1][j-1] != 0)
-                            array[i-1][j-1] = Character.getNumericValue(array[i-1][j-1])+1;
-                        if(array[i-1][j+1] != 0)
-                            array[i-1][j+1] = Character.getNumericValue(array[i-1][j+1])+1;
-                    }catch (IndexOutOfBoundsException e){
-                        e.printStackTrace();
+        int[][] ints = convertStringListTo2DArray(strings);
+        System.out.println(Arrays.deepToString(ints));
+        for (int i = 0; i < ints.length; i++) {
+            int[] intRow = ints[i];
+            System.out.println("Input row: "+Arrays.toString(intRow));
+            if(ArrayUtils.contains(intRow, 0)){
+                for (int j = 0; j < intRow.length; j++) {
+                    int toCheck = intRow[j];
+                    if (toCheck == 0) {
+                        try {
+                            if (ints[i][j + 1] != 0)
+                                ints[i][j + 1] = increaseByOne(ints[i][j + 1]);
+                            if (ints[i][j - 1] != 0)
+                                ints[i][j - 1] = increaseByOne(ints[i][j - 1]);
+                            if (ints[i + 1][j] != 0)
+                                ints[i + 1][j] = increaseByOne(ints[i + 1][j]);
+                            if (ints[i - 1][j] != 0)
+                                ints[i - 1][j] = increaseByOne(ints[i - 1][j]);
+                            if (ints[i + 1][j - 1] != 0)
+                                ints[i + 1][j - 1] = increaseByOne(ints[i + 1][j - 1]);
+                            if (ints[i + 1][j + 1] != 0)
+                                ints[i + 1][j + 1] = increaseByOne(ints[i + 1][j + 1]);
+                            if (ints[i - 1][j - 1] != 0)
+                                ints[i - 1][j - 1] = increaseByOne(ints[i - 1][j - 1]);
+                            if (ints[i - 1][j + 1] != 0)
+                                ints[i - 1][j + 1] = increaseByOne(ints[i - 1][j + 1]);
+                        } catch (IndexOutOfBoundsException e) {
+                            e.printStackTrace();
+                        }
                     }
                 }
             }
+            System.out.println("Output row: "+Arrays.toString(intRow));
         }
-        System.out.print(Arrays.deepToString(array));
+        System.out.print(Arrays.deepToString(ints));
 
-      return null;
+        return convert2DArrayToList(ints);
+    }
+
+    private static int increaseByOne(int i) {
+        return i+1 == 10? 0: i+1;
     }
 
     private static int countFlashes(List<String> strings) {
@@ -99,6 +109,7 @@ public class DumboOctopusPartOne {
         int sum = collect.stream().mapToInt(Integer::parseInt).sum();
         return sum;
     }
+
     private static List<String> increaseEachCharacterByOne(List<String> strings) {
         return strings.stream()
                 .map(String::toCharArray)
@@ -107,7 +118,7 @@ public class DumboOctopusPartOne {
                         char aChar = chars[i];
                         int i1 = Integer.parseInt(String.valueOf(aChar));
                         i1 = i1 + 1;
-                        if(i1>9) i1 = 0;
+                        if (i1 > 9) i1 = 0;
                         chars[i] = String.valueOf(i1).toCharArray()[0];
                     }
                     return chars;
@@ -122,5 +133,25 @@ public class DumboOctopusPartOne {
         for (String s : strings) {
             System.out.println(s);
         }
+    }
+
+    public static int[][] convertStringListTo2DArray(List<String> list) {
+        int[][] ints = new int[list.size()][list.get(0).length()];
+        for (int i = 0; i < ints.length; i++) {
+            char[] chars = list.get(i).toCharArray();
+            for (int j = 0; j < ints[i].length; j++) {
+                ints[i][j] = Character.getNumericValue(chars[j]);
+            }
+        }
+        return ints;
+    }
+
+    public static List<String> convert2DArrayToList(int[][]ints) {
+        List<String>result = new LinkedList<>();
+        for (int i = 0; i < ints.length; i++) {
+            String s = Arrays.toString(ints[i]).replaceAll("[\\]\\[,\\s]", "");
+            result.add(s);
+        }
+        return result;
     }
 }
